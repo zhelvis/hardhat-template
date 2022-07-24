12ebc7df075c293c227c7730a099a6aff1cd09d5
Vladimir Zhelvis <zhelvisvladimir@gmail.com>
2022-07-25 02:31:18 +0300

    add bsc mainnet

--- environment.ts
+++ environment.ts
@@ -3,3 +3,4 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY as string;
+export const BSC_BE_API_KEY = process.env.BSC_BE_API_KEY as string;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import "@nomicfoundation/hardhat-toolbox";
 
 import './tasks/deploy';
 
-import { DEPLOYER_PRIVATE_KEY } from "./environment";
+import { DEPLOYER_PRIVATE_KEY, BSC_BE_API_KEY } from "./environment";
 
 const config = {
   solidity: "0.8.9",
@@ -11,10 +11,19 @@ const config = {
       url: 'http://localhost:8545',
       accounts: [DEPLOYER_PRIVATE_KEY],
     },
-    // TODO: other networks
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: [DEPLOYER_PRIVATE_KEY],
+    },
   },
   gasReporter: {
     enabled: true
+  },
+  etherscan: {
+    apiKey: {
+      bsc: BSC_BE_API_KEY,
+    }
   },
 };
 
