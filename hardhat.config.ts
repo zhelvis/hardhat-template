25784a8014d23fa2b5e901d45a759e65b147bdc8
Vladimir Zhelvis <v.zhelvis@adguard.com>
2023-04-09 00:12:55 +0300

    update deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import "./tasks/deploy";
 import { DEPLOYER_PRIVATE_KEY, BSC_BE_API_KEY } from "./environment";
 
 const config = {
-  solidity: "0.8.9",
+  solidity: "0.8.18",
   networks: {
     local: {
       url: "http://localhost:8545",
